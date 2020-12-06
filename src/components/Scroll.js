8baf96abd9f37c03fea524bf80a88be74f4a80b7
nguyengiahy <nguyengiahy2911@gmail.com>
2020-12-07 00:01:22 +0700

    fix and deploy

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
 	return(
-		<div style={{overflowY: 'scroll', border: '1px solid black', height:'70vh'}}>
+		<div style={{overflowY: 'scroll', border: '1px solid black', height:'65vh'}}>
 			{props.children}
 		</div>
 	);
